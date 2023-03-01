42ab948d571b7903e70f53a261a01f46a4888951
gaurav.tyagi <gaurav.tyagi@calrivate.com>
2023-03-01 19:50:04 +0100

    new file

--- src/test/java/com/tourradar/webtests/steps/Hooks.java
+++ src/test/java/com/tourradar/webtests/steps/Hooks.java
@@ -34,18 +34,7 @@ public class Hooks {
         System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("--no-proxy-server");
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-web-security");
-        options.addArguments("--allow-running-insecure-content");
-        options.addArguments("--ignore-certificate-errors");
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.setExperimentalOption("useAutomationExtension", false);
+
         base.driver = new ChromeDriver(options);
         base.driver.manage().window().maximize();
         base.driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
